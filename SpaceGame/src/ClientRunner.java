2edeceed48778f1869f5eab40620fdbda77df6b4
Zach <edgleyshotfirst@users.noreply.github.com>
2018-02-14 13:53:05 -0600

    Blank Window

--- SpaceGame/src/ClientRunner.java
+++ SpaceGame/src/ClientRunner.java
@@ -1,2 +1,26 @@
-public class ClientRunner {
+
+
+import mayflower.*;
+
+public class ClientRunner extends Mayflower
+{
+
+    private ClientRunner()
+    {
+        super("Spaceman", 1024, 768);
+    }
+
+    @Override
+    public void init()
+    {
+
+        Mayflower.setFullScreen(false);
+        World startingWorld = new StartMenu();
+        Mayflower.setWorld(startingWorld);
+    }
+
+    public static void main(String[] args)
+    {
+        new ClientRunner();
+    }
 }
---
+++ SpaceGame/src/StartMenu.java
@@ -0,0 +1,13 @@
+import mayflower.*;
+
+public class StartMenu extends World {
+    public StartMenu()
+    {
+
+    }
+    @Override
+    public void act()
+    {
+
+    }
+}
