576f299dbeb934930ccfa4c9b914e940e59a5918
Zach <edgleyshotfirst@users.noreply.github.com>
2018-03-02 14:28:20 -0600

    Everything Wraps

--- src/LargeAsteroids.java
+++ src/LargeAsteroids.java
@@ -16,8 +16,25 @@ public class LargeAsteroids extends Asteroids{
     {
         return getIntersectingObjects(Actor.class);
     }
+    public void setPos()
+    {
+        int x = this.getX();
+        int y = this.getY();
+        if(x >= 1008){
+            this.setLocation(0, y);
+        }else if(x <= 0){
+            this.setLocation(1008, y);
+        }else if(y >= 728){
+            this.setLocation(x, 0);
+        }else if(y <= 0){
+            this.setLocation(x, 728);
+        }
+    }
     public void act()
     {
+        if(this.getX()>= (1008-32) || this.getX()<=0+32 || this.getY()>= 728+32 || this.getY()<= 0+32){
+            this.setPos();
+        }
         move(5);
     }
 
